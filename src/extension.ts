4243dcbb4e0f039639e17727628b28b327bf2a17
Aleksandr Shalimov <shalimski@gmail.com>
2021-02-07 11:27:16 +0800

    fix const using

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		else if (opened === 2) {
 			const uri1 = visibleEditors[0].document.uri;
-			const uri2 = vscode.window.visibleTextEditors[1].document.uri;
+			const uri2 = visibleEditors[1].document.uri;
 			vscode.commands.executeCommand("vscode.diff", uri2, uri1);
 		} else {
 			vscode.window.showWarningMessage("More than 2 visible files, can`t swap it :(");
