9b3ddea5443a4e528031f14447c069c4d24f29fd
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-09-24 09:17:01 +0200

    swap "compare active file with clipboard" (#50)
    * swap "compare active file with clipboard"
    * fix: close previous diff, more than 2 files

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,33 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  const documentScheme = ["file", "untitled"];
-
   let disposable = vscode.commands.registerCommand("swapdiff.swapdiff", () => {
-    const files: vscode.Uri[] = [];
-    for (let editor of vscode.window.visibleTextEditors) {
-      if (documentScheme.includes(editor.document.uri.scheme)) {
-        files.push(editor.document.uri);
-      }
+    const tab = vscode.window.tabGroups.activeTabGroup.activeTab;
+    const input = tab?.input;
+    const isDiffTab =
+      input instanceof vscode.TabInputTextDiff ||
+      input instanceof vscode.TabInputNotebookDiff;
+
+    if (!tab || !isDiffTab) {
+      vscode.window.showWarningMessage("Nothing to swap :(");
+      return;
     }
 
-    const opened = files.length;
+    const newLabel = tab.label.split(" ↔ ").reverse().join(" ↔ ");
 
-    if (opened <= 1) {
-      vscode.window.showWarningMessage("Nothing to swap :(");
-    } else if (opened === 2) {
-      const closePrevious = vscode.workspace
-        .getConfiguration()
-        .get("swapdiff.ClosePreviousDiff");
-      if (closePrevious) {
-        vscode.commands.executeCommand("workbench.action.closeActiveEditor");
-      }
+    vscode.commands.executeCommand(
+      "vscode.diff",
+      input.modified,
+      input.original,
+      newLabel
+    );
 
-      vscode.commands.executeCommand("vscode.diff", files[1], files[0]);
-    } else {
-      vscode.window.showWarningMessage(
-        "More than 2 visible files, can`t swap it :("
-      );
+    const closePrevious = vscode.workspace
+      .getConfiguration()
+      .get("swapdiff.ClosePreviousDiff");
+
+    if (closePrevious) {
+      vscode.window.tabGroups.close(tab);
     }
   });
 
